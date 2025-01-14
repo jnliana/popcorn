5995e91b899f4f986e789a34007a271280f67c74
jnliana <jaidith44@hotmail.com>
2025-01-14 12:57:31 -0500

    search text color changed

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -16,7 +16,7 @@ export const Search = ({ query, setQuery }: any) => {
 
   return (
     <input
-      className='justify-self-center border-none px-6 py-2 text-lg rounded-md w-full transition-all duration-300 text-red-300'
+      className='justify-self-center border-none px-6 py-2 text-lg rounded-md w-full transition-all duration-300 text-100'
       type='text'
       placeholder='Search movies...'
       value={query}
